145e9d877fa3b872f66b77453ccf8f24248d4ee6
matt0xFF <github@matt.mieckowski.com>
2020-01-31 23:19:13 -0800

    Working on spiders

---
+++ Assets/Scenes/BugScripts/HammerCollisionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerCollisionHandler : MonoBehaviour
+{
+    public GameObject SpiderPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log(c);
+        var newSpider = Instantiate(SpiderPrefab);
+
+        newSpider.transform.position = transform.position;
+        Vector3 velocity;
+        var minV = -0.5f;
+        var maxV = -minV;
+        velocity.x = Random.Range(minV, maxV);
+        velocity.y = Random.Range(minV, maxV);
+        velocity.z = Random.Range(minV, maxV);
+        newSpider.GetComponent<Rigidbody>().velocity = velocity;
+    }
+
+}
