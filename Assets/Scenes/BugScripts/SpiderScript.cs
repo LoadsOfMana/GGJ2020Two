4a8cf16fde0cbd6cd7788b469d648d30f6126dde
matt0xFF <github@matt.mieckowski.com>
2020-02-01 13:58:50 -0800

    Got spider bouncing working (just adjusting velocity on callback instead of using physics)

---
+++ Assets/Scenes/BugScripts/SpiderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        var f = collider.gameObject.transform.up;
+
+        var r = GetComponent<Rigidbody>();
+        var v = r.velocity;
+        var newV = Vector3.Reflect(v, f);
+
+        Debug.Log("spider hit, up is" + f + " oldV " + v + " newV " + newV);
+
+        // reflect velocity off collision plane
+        r.velocity = newV;
+    }
+}
