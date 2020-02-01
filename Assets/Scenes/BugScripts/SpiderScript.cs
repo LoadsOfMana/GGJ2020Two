813c46751d274f84cbcce056aa02b89549e32ffb
matt0xFF <github@matt.mieckowski.com>
2020-02-01 14:45:39 -0800

    Allow destroying spiders with hammer

--- Assets/Scenes/BugScripts/SpiderScript.cs
+++ Assets/Scenes/BugScripts/SpiderScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpiderScript : MonoBehaviour
 {
+    public float SpawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTime = Time.time;
     }
 
     // Update is called once per frame
@@ -18,15 +20,32 @@ public class SpiderScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
-        var f = collider.gameObject.transform.up;
+        var name = collider.gameObject.name;
 
-        var r = GetComponent<Rigidbody>();
-        var v = r.velocity;
-        var newV = Vector3.Reflect(v, f);
+        if (name.StartsWith("Plane")) {
+            var f = collider.gameObject.transform.up;
 
-        //Debug.Log("spider hit, up is" + f + " oldV " + v + " newV " + newV);
+            var r = GetComponent<Rigidbody>();
+            var v = r.velocity;
+            var newV = Vector3.Reflect(v, f);
 
-        // reflect velocity off collision plane
-        r.velocity = newV;
+            //Debug.Log("spider hit, up is" + f + " oldV " + v + " newV " + newV);
+
+            // reflect velocity off collision plane
+            r.velocity = newV;
+        }
+        else
+        {
+            Debug.Log("hit with " + name + "detected");
+            var aliveTime = Time.time - SpawnTime;
+            // have cooldown before spider can be killed with hammer,
+            // since right now hammer spawns spider it might disappear
+            // right when it's created otherwise
+            if (aliveTime > .5f)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
     }
 }
