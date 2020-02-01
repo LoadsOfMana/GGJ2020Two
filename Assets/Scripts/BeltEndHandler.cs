a4204b6221b3892467a7d6515fac517af537db7d
Colin Creitz <ccreitz@fb.com>
2020-02-01 15:01:50 -0800

    Re-imposing class hierarchy post merge

--- Assets/Scripts/BeltEndHandler.cs
+++ Assets/Scripts/BeltEndHandler.cs
@@ -13,6 +13,26 @@ public class BeltEndHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject incoming = other.gameObject;
+        if(incoming) 
+        {
+            // XXX we need to check incoming for a score, text, audio, etc first
+            Score(incoming.GetComponent<PuzzleWithOutcome>());
+            Destroy(incoming, 5.0f); // XXX this could lead to a race, change to
+                                    // a queued approach?
+        }
+
+    }
+
+    void Score(PuzzleWithOutcome p)
+    {
+        // XXX add logic here to send text to UI, sound to player, score to total
+        Debug.Log(p);
+        return;
     }
 }
