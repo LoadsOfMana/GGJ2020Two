8c7aa949e6f369e3dbd146d186dee7ce6cb25110
LoadsOfMana <loadsofmana@gmail.com>
2020-02-01 07:38:43 -0800

    added conveyors and spawners

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+	public float speed = 1;
+	public List<Rigidbody> touchingBodies;
+	private void OnCollisionEnter(Collision collision)
+	{
+		Rigidbody otherBody = collision.collider.gameObject.GetComponent<Rigidbody>();
+		if (otherBody != null && !touchingBodies.Contains(otherBody))
+			touchingBodies.Add(otherBody);
+	}
+	private void OnCollisionExit(Collision collision)
+	{
+		if (collision.collider.gameObject.GetComponent<Rigidbody>())
+			touchingBodies.Remove(collision.collider.gameObject.GetComponent<Rigidbody>());
+	}
+	private void Update()
+	{
+		if(touchingBodies.Count > 0)
+		{
+			foreach(Rigidbody rb in touchingBodies)
+			{
+				rb.velocity = transform.right * -speed;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PuzzleSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSpawner : MonoBehaviour
+{
+	public GameObject[] puzzles;
+	public int currentPuzzle = 0;
+    
+
+    [ContextMenu("Spawn")]
+	void SpawnNext()
+	{
+		Instantiate(puzzles[currentPuzzle], transform.position, transform.rotation);
+		currentPuzzle++;
+		if (currentPuzzle == puzzles.Length)
+			currentPuzzle = 0;
+	}
+}
