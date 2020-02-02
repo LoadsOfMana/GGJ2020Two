2b5e6e06789ba5362ab4c6a54728aa48b456e7b3
LoadsOfMana <loadsofmana@gmail.com>
2020-02-01 17:20:03 -0800

    stuff

---
+++ Assets/Scripts/RespawnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnTrigger : MonoBehaviour
+{
+	public Transform respawnTar;
+	private void OnTriggerEnter(Collider other)
+	{
+		other.transform.position = respawnTar.position;
+	}
+
+}
---
+++ Assets/Scripts/lazy scripts/RandomVelocity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomVelocity : MonoBehaviour
+{
+	public float variance = 1;
+	private void Start()
+	{
+		GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-variance, variance), Random.Range(-variance, variance), Random.Range(-variance, variance));
+	}
+}
